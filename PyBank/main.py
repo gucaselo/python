1f381978c731de4a5a6825f781044610fa7832aa
gucaselo <gucaselo@gmail.com>
2021-01-14 06:46:01 -0500

    Results properly calculated with code. Missing output file.

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,7 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 #di = dict()
 total_amount = 0
 
-average_change = 0
+total_changes = 0
 increase_profits = None
 decrease_profits = None
 month_increase_profits = None
@@ -39,6 +39,7 @@ with open(csvpath) as csvfile:
                 'If second value is either positive/zero or negative'
                 ' Increase condition'
                 count = count + 1 
+                total_changes = total_changes + (int(values) - a)
                 print('None', count)
 
                 if a < (int(values)): 
@@ -46,12 +47,14 @@ with open(csvpath) as csvfile:
                     decrease_profits = 0
                     month_increase_profits = months
                     a = int(values)  
+                print(total_changes)
                 'Assign value to decrease_profit at start'
                 if a > (int(values)):
                     decrease_profits = (int(values) - a)
                     increase_profits = 0
                     month_decrease_profits = months
-                    a = int(values)                
+                    a = int(values)
+                print(total_changes)                
                 continue
             'Equal Profit/Loss values in consecutive months'
             if a == (int(values)):
@@ -62,18 +65,21 @@ with open(csvpath) as csvfile:
             
             'Profit increased, first and second value is positive'
             if a < (int(values)):
+                total_changes = total_changes + (int(values) - a)
                 count = count + 1
                 print('Increase Count', count)
                 'Substract two values (increase) value' 
                 if (int(values) - (a)) >= (increase_profits) :
                     'Decreased value stored' 
-                    increase_profits = (int(values) - (a))
+                    increase_profits = (int(values) - a)
                     'Month stored' 
                     month_increase_profits = months
-                    'Stored next value for comparison'                                      
+                    'Stored next value for comparison'
+                print(total_changes)                                      
                 a = int(values)
 
             if a > (int(values)):
+                total_changes = total_changes + (int(values) - a)
                 count = count + 1
                 print('Decrease Count', count)
                 'Substract two change values (decrease) value'
@@ -82,6 +88,7 @@ with open(csvpath) as csvfile:
                     decrease_profits = (int(values) - a)
                     month_decrease_profits = months
                 a = int(values)
+                print(total_changes)
 
                 
 
@@ -114,12 +121,12 @@ with open(csvpath) as csvfile:
 
 
     #month_increase_profitss = datetime.strptime(month_increase_profits, '%m-%Y').date()
-
+    print(total_changes)
     print('Financial Analysis')
     print('----------------------------------')
     print('Total Months:', (int(len(pybank))-1))
     print('Total:', '$', total_amount)
-    print('Average Change:', total_amount/(count - 1))) ' Need to update'
+    print('Average Change: ' + '$' + str(total_changes/count)) 
     print('Greatest Increase in Profits: ' + month_increase_profits + ' ($' + str(increase_profits) + ')')
     print('Greatest Decrease in Profits: ' + month_decrease_profits + ' ($' + str(decrease_profits) + ')')
     print(count)
